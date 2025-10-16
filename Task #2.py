c4bdb1cfe71f1af68db1ee4ecd9b44c0d23a4d07
katelinadev <kateryna.kyrychenko.dev.com>
2025-10-16 06:55:36 +0300

    task #2 implementation

---
+++ Task #2.py
@@ -0,0 +1,15 @@
+import random
+
+def get_numbers_ticket(min, max, count):
+    # Перевірка коректності вхідних параметрів
+    if not (min < max and count > 0):
+        return []
+    # Генерація унікальних випадкових чисел
+    numbers = random.sample(range(min, max + 1), count)
+
+    # Повертаємо відсортований список
+    return sorted(numbers)
+
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
