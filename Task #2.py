143ce5db00ab16e6987279e6094b1b3efcd75f41
katelinadev <kateryna.kyrychenko.dev.com>
2025-10-18 00:59:04 +0300

    [task #2] implemented additional check of input data

--- Task #2.py
+++ Task #2.py
@@ -2,7 +2,7 @@ import random
 
 def get_numbers_ticket(min, max, count):
     # Перевірка коректності вхідних параметрів
-    if not (min < max and count > 0):
+    if not (min < max and count > 0 and count <= (max - min + 1)):
         return []
     # Генерація унікальних випадкових чисел
     numbers = random.sample(range(min, max + 1), count)
@@ -10,9 +10,11 @@ def get_numbers_ticket(min, max, count):
     numbers.sort()
     print("Відсортована послідовність:", numbers);
     # Повертаємо відсортований список
-    #return sorted(numbers)
     return numbers
 
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+#lottery_numbers = get_numbers_ticket(1, 49, 6)
+#lottery_numbers = get_numbers_ticket(-10, 10, 5)
+#lottery_numbers = get_numbers_ticket(1000, 1200, 10)
+lottery_numbers = get_numbers_ticket(10, 14, 6)  # Некоректний випадок
 print("Ваші лотерейні числа:", lottery_numbers)
